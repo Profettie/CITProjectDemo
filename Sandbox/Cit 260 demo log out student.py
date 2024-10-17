c055c37e136f6436a786ebf3d16e51bfa388fb73
Profettie <Matthewproctor2007@gmail.com>
2024-10-16 19:14:57 -0700

    Add files via upload

---
+++ Sandbox/Cit 260 demo log out student.py
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset = "UTF-8">
+        <meta name= "viewport" content="width=device-width, intial-scale=1.0">
+        <title> Log out </title>
+    </head>
+    <body>
+        <h1>Log out so soon?</h1>
+        <hr />
+        <hr />
+        <ul>
+            <a href="https://tject.github.io/CITProjectDemo/index.html">Log out</a>
+            
+    </body>
+
+</html>
